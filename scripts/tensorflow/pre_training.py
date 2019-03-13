5c871b70256378dea2b098c2f778eb381045b474
jimmy <jimmy60504@gmail.com>
2019-03-14 04:12:40 +0800

    support Tensorflow 2.0

--- scripts/tensorflow/pre_training.py
+++ scripts/tensorflow/pre_training.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow import keras
+from tensorflow.python import keras
 from tensorflow.python.keras.optimizers import Adam
 from tensorflow.python.keras.utils import multi_gpu_model
 
@@ -16,10 +16,8 @@ validation_generator = DataGenerator(pkl_list[split_point:], batch_size=2)
 
 tensorboard = keras.callbacks.TensorBoard(log_dir='../logs', histogram_freq=0,
                                           write_graph=True, write_images=False)
-with tf.device('/cpu:0'):
-    model = Nest_Net(1, 3001, 1)
 
-model = multi_gpu_model(model, gpus=2)
+model = Nest_Net(1, 3001, 1)
 model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
 model.fit_generator(generator=training_generator, validation_data=validation_generator,
                     epochs=1, use_multiprocessing=True, callbacks=[tensorboard])
