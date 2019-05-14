d754eebedbd56d366fb716a342739f685e2fa046
jimmy60504 <jimmy60504@gmail.com>
2019-05-15 07:26:33 +0800

    change input from list to catalog

--- scripts/generate_dataset.py
+++ scripts/generate_dataset.py
@@ -1,8 +1,14 @@
-from seisnn.io import write_training_pkl
+from seisnn.io import read_event_list, read_pickle, write_pickle, write_training_pkl
 
 sds_root = "/mnt/DATA"
-sfile_list = ["/mnt/tf_data/sfile/201718select.out"]
-pkl_dir = "/mnt/tf_data/pkl/201718select"
+sfile_list = ["/mnt/tf_data/catalog/201718select.out"]
+pkl_dir = "/mnt/tf_data/pkl/201718select_random"
+catalog_pkl = "/mnt/tf_data/catalog/saved_catalog.pkl"
 
-write_training_pkl(sfile_list, sds_root, pkl_dir=pkl_dir, remove_dir=True)
+# catalog = read_event_list(sfile_list)
+# write_pickle(catalog, catalog_pkl)
 
+catalog = read_pickle(catalog_pkl)
+cat = catalog.filter("time >= 2018-02-15")
+
+# write_training_pkl(cat, sds_root, pkl_dir=pkl_dir, random_time=60, remove_dir=True)
