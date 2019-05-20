763aa5f45e484f47c12393fbdae98c0d4de3d23c
jimmy60504 <jimmy60504@gmail.com>
2019-05-20 13:25:54 +0800

    add requirements

--- setup.py
+++ setup.py
@@ -8,14 +8,21 @@ setup(
     name='SeisNN',
     packages=['seisnn'],
     version='0.0.1dev1',
-    description='Deep learning seismic picking package',
+    description='Deep learning seismic phase picking package',
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license='MIT',
     author='jimmy',
     author_email='jimmy60504@gmail.com',
     url='https://github.com/jimmy60504/SeisNN',
-    python_requires='>=3',
+    python_requires='>=3.5',
+    install_requires=[
+        'matplotlib>=3.0.3',
+        'numpy>=1.16.2',
+        'scipy>=1.2.1',
+        'obspy>=1.1.1'
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
     ]
 )
