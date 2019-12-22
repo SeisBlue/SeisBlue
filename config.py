1da1b3c9b0d13542998900f7d087612a70febe91
jimmy <jimmy60504@gmail.com>
2019-12-22 15:39:39 +0800

    add geom dir

--- config.py
+++ config.py
@@ -6,25 +6,29 @@ from seisnn.utils import make_dirs
 SDS_ROOT = '/mnt/SDS_ROOT'
 WORKSPACE = os.path.expanduser('~')
 
-
 TFRECORD_ROOT = os.path.join(WORKSPACE, 'tfrecord')
 DATASET_ROOT = os.path.join(TFRECORD_ROOT, 'dataset')
-CATALOG_ROOT = os.path.join(TFRECORD_ROOT, 'catalog')
 PICK_ROOT = os.path.join(TFRECORD_ROOT, 'picks')
 
+CATALOG_ROOT = os.path.join(WORKSPACE, 'catalog')
+GEOM_ROOT = os.path.join(WORKSPACE, 'geom')
 MODELS_ROOT = os.path.join(WORKSPACE, 'models')
+
 config = {'SDS_ROOT': SDS_ROOT,
           'WORKSPACE': WORKSPACE,
+
           'TFRECORD_ROOT': TFRECORD_ROOT,
           'DATASET_ROOT': DATASET_ROOT,
-          'CATALOG_ROOT': CATALOG_ROOT,
           'PICK_ROOT': PICK_ROOT,
+
+          'CATALOG_ROOT': CATALOG_ROOT,
+          'GEOM_ROOT': GEOM_ROOT,
           'MODELS_ROOT': MODELS_ROOT,
           }
 
 # mkdir for all folders and store into config.yaml
 if __name__ == '__main__':
-    for d in [TFRECORD_ROOT, DATASET_ROOT, PICK_ROOT, MODELS_ROOT]:
+    for d in [TFRECORD_ROOT, DATASET_ROOT, PICK_ROOT, CATALOG_ROOT, MODELS_ROOT, GEOM_ROOT]:
         make_dirs(d)
 
     with open('config.yaml', 'w') as file:
