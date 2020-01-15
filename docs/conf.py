540d908d596c2fa46f34a92c46c054eb6b7fedae
Jimmy <jimmy60504@gmail.com>
2020-01-15 14:34:00 +0800

    add Sphinx Bootstrap Theme

--- docs/conf.py
+++ docs/conf.py
@@ -10,8 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
+import sphinx_bootstrap_theme
+sys.path.append(os.path.abspath('../seisnn'))
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -45,7 +47,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
