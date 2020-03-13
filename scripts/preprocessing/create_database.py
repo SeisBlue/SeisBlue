fbcb682f2b0af7d3ea82f30e2f7d58bb36ca1c14
jimmy60504@gmail.com <jimmy60504@gmail.com>
2020-03-14 05:21:00 +0800

    [skip ci] add pickset

--- scripts/preprocessing/create_database.py
+++ scripts/preprocessing/create_database.py
@@ -5,9 +5,10 @@ from seisnn.pick import get_pick_dict
 from seisnn.database import Client
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-d', '--database', required=True, help='database file', type=str)
+ap.add_argument('-d', '--database', required=True, help='sql database file', type=str)
 ap.add_argument('-c', '--catalog', required=True, help='catalog s-file dir', type=str)
 ap.add_argument('-g', '--geometry', required=True, help='geometry STATION0.HYP', type=str)
+ap.add_argument('-p', '--pickset', required=True, help='output pickset name', type=str)
 args = ap.parse_args()
 
 geom = read_hyp(args.geometry)
@@ -16,4 +17,4 @@ pick_dict = get_pick_dict(events)
 
 db = Client(args.database)
 db.add_geom(geom)
-db.add_picks(pick_dict, 'manual')
+db.add_picks(pick_dict, args.pickset)
