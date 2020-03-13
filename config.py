bc169b767c2f8503073a526ab80eff316f8a90ba
jimmy60504@gmail.com <jimmy60504@gmail.com>
2020-03-14 05:21:17 +0800

    [skip ci] add database

--- config.py
+++ config.py
@@ -8,8 +8,7 @@ SDS_ROOT = '/mnt/SDS_ROOT'
 WORKSPACE = os.path.expanduser('~')
 
 TFRECORD_ROOT = os.path.join(WORKSPACE, 'tfrecord')
-DATASET_ROOT = os.path.join(TFRECORD_ROOT, 'dataset')
-PICK_ROOT = os.path.join(TFRECORD_ROOT, 'picks')
+DATABASE_ROOT = os.path.join(WORKSPACE, 'database')
 
 CATALOG_ROOT = os.path.join(WORKSPACE, 'catalog')
 GEOM_ROOT = os.path.join(WORKSPACE, 'geom')
@@ -19,8 +18,7 @@ config = {'SDS_ROOT': SDS_ROOT,
           'WORKSPACE': WORKSPACE,
 
           'TFRECORD_ROOT': TFRECORD_ROOT,
-          'DATASET_ROOT': DATASET_ROOT,
-          'PICK_ROOT': PICK_ROOT,
+          'DATABASE_ROOT': DATABASE_ROOT,
 
           'CATALOG_ROOT': CATALOG_ROOT,
           'GEOM_ROOT': GEOM_ROOT,
@@ -29,7 +27,7 @@ config = {'SDS_ROOT': SDS_ROOT,
 
 # mkdir for all folders and store into config.yaml
 if __name__ == '__main__':
-    for d in [TFRECORD_ROOT, DATASET_ROOT, PICK_ROOT, CATALOG_ROOT, MODELS_ROOT, GEOM_ROOT]:
+    for d in [TFRECORD_ROOT, DATABASE_ROOT, CATALOG_ROOT, MODELS_ROOT, GEOM_ROOT]:
         make_dirs(d)
 
     with open(os.path.join(expanduser("~"), '.bashrc'), 'w') as file:
@@ -37,3 +35,5 @@ if __name__ == '__main__':
 
     with open('config.yaml', 'w') as file:
         yaml.dump(config, file, sort_keys=False)
+
+    print('SeisNN initialized.')
