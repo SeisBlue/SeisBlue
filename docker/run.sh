249374849f25db5ede8a2a919b5cfc455d0977f1
jimmy60504@gmail.com <jimmy60504@gmail.com>
2020-04-15 23:02:21 +0800

    [skip ci] add port and change path

--- docker/run.sh
+++ docker/run.sh
@@ -2,12 +2,13 @@ docker container rm -f tf_ssh
 docker run -d \
         --gpus all \
         -p 49154:22 \
-        -p 0.0.0.0:6006:6006 \
+        -p 8888:8888 \
+        -p 6006:6006 \
         -v /etc/passwd:/etc/passwd:ro \
         -v /etc/shadow:/etc/shadow:ro \
         -v /etc/group:/etc/group:ro \
         -v </path/to/seisnn>:/SeisNN \
         -v </path/to/workspace>:/home/${USER} \
-        -v </path/to/sfile>:/mnt/sfile \
-        -v </path/to/database>:/mnt/SDS_ROOT:ro \
+        -v </path/to/sfile>:/home/${USER}/SFILE_ROOT \
+        -v </path/to/database>:/home/${USER}/SDS_ROOT:ro \
         --name tf_ssh seisnn/tf_ssh
