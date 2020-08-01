39c3a7e53b6a59788e4961364d25a7ac50ca2dd0
Jimmy <jimmy60504@gmail.com>
2020-08-02 01:53:12 +0800

    change dataset dir to file name

--- scripts/visualization/plot_instance.py
+++ scripts/visualization/plot_instance.py
@@ -1,14 +1,8 @@
-import os
-
 import seisnn
 
-dataset = 'HL2017'
-
-config = seisnn.utils.get_config()
-dataset_dir = os.path.join(config['TFRECORD_ROOT'], dataset)
-dataset = seisnn.data.io.read_dataset(dataset_dir)
+dataset = seisnn.data.io.read_dataset('HL2017')
 
 for batch in dataset.shuffle(1000).batch(2):
     for example in seisnn.data.example_proto.batch_iterator(batch):
-        feature = seisnn.data.core.Instance(example)
-        feature.plot(enlarge=True, snr=True)
+        instance = seisnn.data.core.Instance(example)
+        instance.plot(enlarge=True, snr=True)
