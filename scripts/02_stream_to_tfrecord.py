cb3e8caace32e63a92a477ccbcee91fb547f0d31
Jimmy <jimmy60504@gmail.com>
2020-08-02 05:29:37 +0800

    add waveform_summary

--- scripts/02_stream_to_tfrecord.py
+++ scripts/02_stream_to_tfrecord.py
@@ -1,6 +1,9 @@
 import seisnn
 
+dataset = 'HL2017'
 db = seisnn.data.sql.Client('test.db')
 db.pick_summery()
 
-db.generate_training_data('HL2017')
+db.generate_training_data(dataset)
+db.sync_dataset_header(dataset)
+db.waveform_summery()
--- scripts/visualization/plot_instance.py
+++ scripts/visualization/plot_instance.py
@@ -2,7 +2,6 @@ import seisnn
 
 dataset = seisnn.data.io.read_dataset('HL2017')
 
-for batch in dataset.shuffle(1000).batch(2):
-    for example in seisnn.data.example_proto.batch_iterator(batch):
-        instance = seisnn.data.core.Instance(example)
-        instance.plot(enlarge=True, snr=True)
+for example in dataset:
+    instance = seisnn.data.core.Instance(example)
+    instance.plot(enlarge=True, snr=True)
