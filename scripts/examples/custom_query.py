83c1f3968ca0f83ad1c9516128d083006250a17b
Jimmy <jimmy60504@gmail.com>
2020-08-06 04:17:20 +0800

    add filter time

--- scripts/examples/custom_query.py
+++ scripts/examples/custom_query.py
@@ -5,7 +5,7 @@ import seisnn
 # connect to sql database
 db = seisnn.data.sql.Client('HL2017.db')
 
-# open sql session, it will terminate session when exit `with` statement
+# open sql session, it will terminate the session when exit `with` statement
 with db.session_scope() as session:
 
     # get related table class from name
@@ -16,8 +16,8 @@ with db.session_scope() as session:
 
     # filter wanted criteria
     query = query.filter(Pick.phase.like('P'))
-    query = query.filter(Pick.time >= datetime.datetime(2017, 1, 23))
-    query = query.filter(Pick.time <= datetime.datetime(2017, 1, 24))
+    query = query.filter(Pick.time >= datetime.datetime(2017, 1, 23, 13, 2, 7))
+    query = query.filter(Pick.time <= datetime.datetime(2017, 1, 23, 13, 2, 8))
 
 # use .all() to make query into a list and print each row
 for row in query.all():
