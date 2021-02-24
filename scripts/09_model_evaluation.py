83cdf8396e6ab638acc46f8bc2c8decc9b596700
andy <qwert159784623@gmail.com>
2021-02-24 21:46:17 +0800

    evaluator.score(delta = 0.1)

--- scripts/09_model_evaluation.py
+++ scripts/09_model_evaluation.py
@@ -1,7 +1,8 @@
 import seisnn
 
-test_set = 'HL2017'
+test_set = 'HL2019test'
 model_instance = 'test_model'
-database = 'HL2017.db'
+database = 'HL2019.db'
 
 evaluator = seisnn.model.evaluator.GeneratorEvaluator(database, model_instance)
+evaluator.score(delta=0.1)
