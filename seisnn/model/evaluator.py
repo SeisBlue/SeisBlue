94258e039b10f4e5863ac7e27013209b56a58f18
andy <qwert159784623@gmail.com>
2021-02-24 20:02:45 +0800

    fix eval

--- seisnn/model/evaluator.py
+++ seisnn/model/evaluator.py
@@ -89,12 +89,12 @@ class GeneratorEvaluator(BaseEvaluator):
         for val in dataset.prefetch(100).batch(batch_size):
             progbar.add(batch_size)
 
-            title = f"eval_{n:0>5}"
             val['predict'] = self.model.predict(val['trace'])
-            val['id'] = tf.convert_to_tensor(
-                title.encode('utf-8'), dtype=tf.string)[tf.newaxis]
+            iterator = seisnn.example_proto.batch_iterator(val)
+            for i in range(len(val['predict'])):
+                title = f"eval_{n:0>5}"
 
-            example = next(seisnn.example_proto.batch_iterator(val))
-            instance = Instance(example)
-            instance.to_tfrecord(os.path.join(eval_path, title + '.tfrecord'))
-            n += 1
+                instance = Instance(next(iterator))
+                instance.to_tfrecord(
+                    os.path.join(eval_path, title + '.tfrecord'))
+                n += 1
