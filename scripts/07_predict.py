2baf6de63f2ba54d9fa895b00dbf276a4db0a51d
andy <qwert159784623@gmail.com>
2021-04-13 11:23:21 +0800

    change the way to get tfr_list eval rename to predict

--- scripts/07_predict.py
+++ scripts/07_predict.py
@@ -4,7 +4,7 @@ model = 'test_model.h5'
 database = 'Hualien.db'
 
 db = seisnn.sql.Client(database)
-tfr_list = db.get_matched_list('*', 'tfrecord', 'path')
+tfr_list = db.get_tfrecord(from_date='2019-05-09',column='path')
 
 evaluator = seisnn.model.evaluator.GeneratorEvaluator(database, model)
 evaluator.eval(tfr_list)
