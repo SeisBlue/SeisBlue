64fbe74145263f6bdade3ac2f01851cf0420aefe
andy <qwert159784623@gmail.com>
2021-04-15 12:05:44 +0800

    can change height

--- scripts/09_model_evaluation.py
+++ scripts/09_model_evaluation.py
@@ -4,5 +4,6 @@ model_instance = 'test_model'
 database = 'Hualien.db'
 evaluator = seisnn.model.evaluator.GeneratorEvaluator(database, model_instance)
 tfr_list = evaluator.get_eval_list()
-evaluator.score(tfr_list, delta=0.5, error_distribution=True)
+evaluator.score(tfr_list, height=0.5,delta=0.1, error_distribution=True)
+
 
