e17fcee38166c0eb9dc363d5ae0ae55da243f7ca
andy <qwert159784623@gmail.com>
2021-05-10 16:11:13 +0800

    read geom and afile to database

--- scripts/01_load_sql_database.py
+++ scripts/01_load_sql_database.py
@@ -3,17 +3,18 @@ import seisnn
 db = seisnn.sql.Client(database="tt.db")
 inspector = seisnn.sql.DatabaseInspector(db)
 
-# db.read_nsta(nsta="nsta24.dat")
-# inspector.inventory_summery()
-
-db.read_hyp(hyp="HL2019.HYP", network="HL")
+db.read_GDMSstations(nsta="GDMSstations.csv")
 inspector.inventory_summery()
 
+# db.read_hyp(hyp="HL2019.HYP", network="HL")
+# inspector.inventory_summery()
 
-# path_list = '/home/andy/A_file/*'
-# events = seisnn.io.read_afile_directory(path_list)
 
-db.add_events(catalog="HL2019", tag="manual")
+path_list = '/home/andy/A_file/*'
+events = seisnn.io.read_afile_directory(path_list)
+
+db.add_sfile_events(events=events, tag="manual")
+
 inspector.event_summery()
 inspector.pick_summery()
 
