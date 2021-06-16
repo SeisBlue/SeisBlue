2df63d2938bc4f224fa26db4e2a96bf3ee77b11e
andy <qwert159784623@gmail.com>
2021-06-16 21:08:42 +0800

    tfr_list = seisnn.utils.flatten_list(tfr_list)

--- scripts/07_predict.py
+++ scripts/07_predict.py
@@ -4,7 +4,8 @@ model = 'CWB_2010_2019_transgan.h5'
 database = 'CWB.db'
 
 db = seisnn.sql.Client(database)
-tfr_list = db.get_tfrecord(from_date='2020-01-01',column='path')
+tfr_list = db.get_tfrecord(from_date='2020-01-01',to_date='2020-12-31',column='path')
+tfr_list = seisnn.utils.flatten_list(tfr_list)
 
 evaluator = seisnn.model.evaluator.GeneratorEvaluator(database, model)
 evaluator.predict(tfr_list)
